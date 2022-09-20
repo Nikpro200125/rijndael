16d75dbb87e98aa991335d69398efe7623b43af5
Nikita Prokopyev <Nikpro200125@gmail.com>
2022-09-21 02:01:36 +0300

    refactoring

--- src/main/kotlin/Rijndael.kt
+++ src/main/kotlin/Rijndael.kt
@@ -1,8 +1,12 @@
 import java.io.File
-import java.util.*
 import kotlin.system.exitProcess
 
-class Rijndael(input: File) {
+// Константы
+const val Nb = 4
+const val Nk = 4
+const val Nr = 10
+
+class Rijndael(input: File, password: String) {
     val text: ByteArray // Исходный файл в байтах
     val input: File // Файл ввода
     val output: File // Файл вывода
@@ -35,11 +39,11 @@ class Rijndael(input: File) {
         // Ввод пароля и создание из него 128 bit ключа
         println("Введите пароль...")
         val tmp = ByteArray(4)
-        val password = Scanner(System.`in`).next().hashCode()
-        tmp[0] = (password shr 0).toByte()
-        tmp[1] = (password shr 8).toByte()
-        tmp[2] = (password shr 16).toByte()
-        tmp[3] = (password shr 24).toByte()
+        val hashPassword = password.hashCode()
+        tmp[0] = (hashPassword shr 0).toByte()
+        tmp[1] = (hashPassword shr 8).toByte()
+        tmp[2] = (hashPassword shr 16).toByte()
+        tmp[3] = (hashPassword shr 24).toByte()
         for (i in 0 until Nk) {
             for (j in 0 until 4) {
                 key[4 * i + j] = tmp[j]
@@ -47,7 +51,16 @@ class Rijndael(input: File) {
         }
         // Разбиение текста по блокам
         for (i in text.indices step 4 * Nb) {
-            blocks.add(Block(text.sliceArray(IntRange(i, if ((i + 4 * Nb - 1) < text.size) (i + 4 * Nb - 1) else (text.size - 1))), key))
+            blocks.add(
+                Block(
+                    text.sliceArray(
+                        IntRange(
+                            i,
+                            if ((i + 4 * Nb - 1) < text.size) (i + 4 * Nb - 1) else (text.size - 1)
+                        )
+                    ), key
+                )
+            )
         }
     }
 
