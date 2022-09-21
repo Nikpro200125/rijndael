1ddb99d5096b8735da12a06feba4dfb1f33f06ac
Nikita Prokopyev <Nikpro200125@gmail.com>
2022-09-21 17:00:18 +0300

    realised key and block sizes in (128, 192, 256)bit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,10 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-
     // Ввод пути к исходному файлу и инициализация алгоритма
     println("Введите путь к исходному файлу...")
-    val cipher = Rijndael(File(scanner.next()))
-
-    // Ввод режима работы и запуск необходимого режима
+    val cipher = Rijndael(File(scanner.next()), "a", 8, 4)
+    //Ввод режима работы и запуск необходимого режима
     println("Введите режим 1 - шифрование, 2 - дешифрация...")
     when (scanner.nextInt()) {
         1    -> cipher.encode()
--- src/main/kotlin/Rijndael.kt
+++ src/main/kotlin/Rijndael.kt
@@ -1,32 +1,92 @@
 import java.io.File
+import kotlin.experimental.xor
 import kotlin.system.exitProcess
 
-// Константы
-const val Nb = 4
-const val Nk = 4
-const val Nr = 10
+// Параметры алгоритма
+var Nb = 4
+var Nk = 4
+var Nr = 10
 
-class Rijndael(input: File, password: String) {
-    val text: ByteArray // Исходный файл в байтах
-    val input: File // Файл ввода
-    val output: File // Файл вывода
-    val blocks = mutableListOf<Block>() // Блок в виде таблицы в байтах
-    var key = ByteArray(4 * Nk) // Ключ
+private val rCon = arrayOf(
+    byteArrayOf(0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x01.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x02.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x04.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x08.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x10.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x20.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x40.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x80.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x1b.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x36.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x6c.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xd8.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xab.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x4d.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x9a.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x2f.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x5e.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xbc.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xc6.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x97.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x35.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x6a.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xd4.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xb3.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x7d.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xfa.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xef.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0xc5.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x91.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x39.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+    byteArrayOf(0x72.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte()),
+)
+
+class Rijndael(input: File, password: String, nb: Int, nk: Int) {
+    private val text: ByteArray // Исходный файл в байтах
+    private val input: File // Файл ввода
+    private val output: File // Файл вывода
+    private val blocks = mutableListOf<Block>() // Блок в виде таблицы в байтах
+    private var key: ByteArray // Ключ
+    private var extendedKey: MutableList<ByteArray> = mutableListOf() // Расширенный ключ
 
     init {
-        // Проверка входногл файла
+        // Проверка размера блока
+        if (nb !in listOf(4, 6, 8)) {
+            print("Неверный размер блока")
+            exitProcess(1)
+        }
+        Nb = nb
+
+        // Проверка размера ключа
+        if (nk !in listOf(4, 6, 8)) {
+            print("Неверный размер ключа")
+            exitProcess(6)
+        }
+        Nk = nk
+
+        // Выбор количества раундов
+        if (Nb == 8 || Nk == 8) {
+            Nr = 14
+        } else if (Nb == 6 || Nk == 6) {
+            Nr = 12
+        }
+
+        // Проверка входного файла
         this.input = input
         if (!input.exists() || !input.isFile || !input.canRead()) {
             print("Файл не существует или у вас недостаточно прав!")
             exitProcess(2)
         }
+
         // Проверка второго файла
         this.output = File(input.parent + "\\out." + input.name)
         if (!output.exists() && !output.createNewFile() || !output.canWrite()) {
             print("Не удалось создать выходной файл!")
             exitProcess(3)
         }
-        // Опустошение файла
+
+        // Опустошение выходного файла
         output.writeText("")
 
         // Ввод исходного файла и проверка на пустоту
@@ -37,30 +97,78 @@ class Rijndael(input: File, password: String) {
         }
 
         // Ввод пароля и создание из него 128 bit ключа
-        println("Введите пароль...")
         val tmp = ByteArray(4)
         val hashPassword = password.hashCode()
         tmp[0] = (hashPassword shr 0).toByte()
         tmp[1] = (hashPassword shr 8).toByte()
         tmp[2] = (hashPassword shr 16).toByte()
         tmp[3] = (hashPassword shr 24).toByte()
+        key = ByteArray(4 * Nk)
         for (i in 0 until Nk) {
             for (j in 0 until 4) {
                 key[4 * i + j] = tmp[j]
             }
         }
+        // Генерация расщиренного ключа
+        keyExpansion()
+
         // Разбиение текста по блокам
         for (i in text.indices step 4 * Nb) {
             blocks.add(
                 Block(
-                    text.sliceArray(
-                        IntRange(
-                            i,
-                            if ((i + 4 * Nb - 1) < text.size) (i + 4 * Nb - 1) else (text.size - 1)
-                        )
-                    ), key
+                    text.sliceArray(IntRange(i, if ((i + 4 * Nb - 1) < text.size) (i + 4 * Nb - 1) else (text.size - 1))),
+                    extendedKey
                 )
             )
+        }
+    }
+
+    // сдвиг слова по вертикали на один байт влево
+    private fun rotWord(n: Int) {
+        val tmp = extendedKey[0][n]
+        for (i in 0 until extendedKey.size - 1) {
+            extendedKey[i][n] = extendedKey[i + 1][n]
+        }
+        extendedKey[extendedKey.size - 1][n] = tmp
+    }
+
+    // Замена байт ключа через таблицу S-box
+    private fun byteSubWord(n: Int) {
+        for (i in 0 until extendedKey.size) {
+            extendedKey[i][n] = search(extendedKey[i][n], 0)
+        }
+    }
+
+    // Генерация ключей для всех раундов
+    private fun keyExpansion() {
+        repeat(4) {
+            extendedKey.add(ByteArray(Nb * (Nr + 1)))
+        }
+        for (i in 0 until Nk) {
+            for (j in 0 until 4) {
+                extendedKey[j][i] = key[4 * i + j]
+            }
+        }
+        for (i in Nk until Nb * (Nr + 1)) {
+            for (j in 0 until extendedKey.size) {
+                extendedKey[j][i] = extendedKey[j][i - 1]
+            }
+            if (i % Nk == 0) {
+                rotWord(i)
+                byteSubWord(i)
+                for (j in 0 until extendedKey.size) {
+                    extendedKey[j][i] = extendedKey[j][i - Nk].xor(extendedKey[j][i]).xor(rCon[i / Nk - 1][j])
+                }
+            } else if (Nk > 6 && i % Nk == 4) {
+                byteSubWord(i)
+                for (j in 0 until extendedKey.size) {
+                    extendedKey[j][i] = extendedKey[j][i - Nk].xor(extendedKey[j][i])
+                }
+            } else {
+                for (j in 0 until extendedKey.size) {
+                    extendedKey[j][i] = extendedKey[j][i - Nk].xor(extendedKey[j][i])
+                }
+            }
         }
     }
 
